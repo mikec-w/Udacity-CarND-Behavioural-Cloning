46c1e7266e6fd7a50657b42efa34b3c2d82e68bc
mikec-w <mikecw@gmail.com>
2020-05-05 09:50:24 +0000

    Changes:
    * Model.py - added code to load images and CSV data as generators
    * Model.py - added basic model for initial testing
    * Download.sh - basic script for pulling data from OneDrive

---
+++ download.sh
@@ -0,0 +1,12 @@
+# Download training data for the Behavioural Cloning Project
+# Note data will be lost when workspace is reset
+
+# Put data in /opt/data
+cd /opt/
+mkdir data
+
+# Get data from onedrive (mikecw@gmail.com account)
+wget --no-check-certificate "https://onedrive.live.com/download?cid=6EBB03E38A53ED3E&resid=6EBB03E38A53ED3E%21116&authkey=AC4lDqtLG8LqfiA"
+
+# Unzip 
+

ff1b7cdde2f73a341f86e01912dfbc1398298aba
mikec-w <mikecw@gmail.com>
2020-05-05 13:32:31 +0000

    Changes:
    * Model loads sample data and runs basic network
    * Generators all work and are tested
    * Download script edited to pull sample training data from OneDrive
    * First run of model.h5 added

--- download.sh
+++ download.sh
@@ -2,11 +2,20 @@
 # Note data will be lost when workspace is reset
 
 # Put data in /opt/data
-cd /opt/
-mkdir data
+
+mkdir /opt/data
+cd /opt/data
 
 # Get data from onedrive (mikecw@gmail.com account)
-wget --no-check-certificate "https://onedrive.live.com/download?cid=6EBB03E38A53ED3E&resid=6EBB03E38A53ED3E%21116&authkey=AC4lDqtLG8LqfiA"
+
+# Sample Data link
+wget -O sample_data.zip --no-check-certificate "https://onedrive.live.com/download?cid=B7BE61D821A6E0C4&resid=B7BE61D821A6E0C4%21148&authkey=AEpzRSDtPGdZT3s"
+unzip sample_data.zip
+rm -r __MACOSX/
+rm sample_data.zip
+
+# User Generated Data link
+# wget --no-check-certificate "https://onedrive.live.com/download?cid=6EBB03E38A53ED3E&resid=6EBB03E38A53ED3E%21116&authkey=AC4lDqtLG8LqfiA"
 
 # Unzip 
 
--- model.py
+++ model.py
@@ -1,13 +1,14 @@
 ## Model for Autonomous Driving
 
 # Import Library
-from keras.models import Sequential
-from keras.layers import Flatten, Dense
 import csv
 import cv2 
 import os
 import numpy as np
-
+from sklearn.utils import shuffle
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from sklearn.model_selection import train_test_split
 
 # Based on the NVidia Deep Learning Network
 
@@ -15,53 +16,60 @@ import numpy as np
 # Use the download script...
 
 # Load Data to start
-lines = []
-with open('/opt/data/driving_log.csv') as csvfile:
+samples = []
+with open('/opt/data/data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
+    # skip header
+    next(reader)
     for line in reader:
-        lines.append(line)
+        samples.append(line)
+        
+       
+# Split Training and validation set
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
         
 # Use a generator to load the images as required rather than as a whole set
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1: # loop forever so generator never terminates
         shuffle(samples)
-        for offset in range(0, num_samples, batch_size)
-            batch_samples = smaples[offset:offset+batch_size]
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
             
             images = []          
             measurements = []
             for batch_sample in batch_samples:
-                name = '/opt/data/IMG/' + batch_sample[0].split('/')[-1]
+                name = '/opt/data/data/IMG/' + batch_sample[0].split('/')[-1]
                 image = cv2.imread(name)
                 images.append(image)
-                # Get steering wheel measurement
+                # Get steering wheel measurement (4th column)
                 measurements.append(float(batch_sample[3]))
     
     
             X_train = np.array(images)
             y_train = np.array(measurements)
-            yield sklearn.utils.shuffle(X_train, y_train)
+            yield shuffle(X_train, y_train)
 
 # Generator functions
-train_generator = generator(train_samples, batch_size=32)
-validation_generator = generator(validation_samples, batch_size=32)
+batch_size=32
+train_generator = generator(train_samples, batch_size=batch_size)
+validation_generator = generator(validation_samples, batch_size=batch_size)
 
 # Start with Basic Model to get it all running (regression network)
 model = Sequential()
 
 # Preprocessing (normalize & crop image)
-model.add(lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320,3)))
-model.add(Cropping2D(cropping=((50,20),(0,0)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320,3)))
+model.add(Cropping2D(cropping=((50,20),(0,0))))
 # Model
 
 model.add(Flatten())
-model.add(Dense(1)
+model.add(Dense(1))
            
 model.compile(loss='mse', optimizer='adam')
 #model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
-model.fit_generator(train_generator, steps_per_epoch=ceil(len(train_samples)/batch_size), /
-                    validation_generator, validation_steps=(len(validation_samples)/batch_size), /
+model.fit_generator(train_generator, steps_per_epoch=np.ceil(len(train_samples)/batch_size), \
+                    validation_data=validation_generator, validation_steps=np.ceil(len(validation_samples)/batch_size), \
                     epochs=5, verbose=1)
           
 model.save('model.h5')
